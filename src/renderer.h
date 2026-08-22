12453a7dc90c4d1883c5cd025e507bd5df72431a
Maple_Miles <maplemilesalt@gmail.com>
2026-08-22 10:43:47 -0300

    Add guest framebuffer rendering

--- src/renderer.h
+++ src/renderer.h
@@ -1,22 +1,18 @@
 #pragma once
-
 #include <cstdint>
 #include <vector>
-
+class GuestMemory;
 class Renderer {
 public:
-    Renderer(int width, int height);
+    Renderer(int width,int height);
     void clear(std::uint32_t color);
     void drawCube(float angle);
+    bool drawGuestFramebuffer(const GuestMemory& memory,std::uint64_t address,int guestWidth,int guestHeight);
     const std::uint32_t* pixels() const;
-    int width() const;
-    int height() const;
-
+    int width() const; int height() const;
 private:
-    struct Vertex { float x, y, z; };
-    void putPixel(int x, int y, std::uint32_t color);
-    void drawLine(int x0, int y0, int x1, int y1, std::uint32_t color);
-    int width_;
-    int height_;
-    std::vector<std::uint32_t> pixels_;
+    struct Vertex{float x,y,z;};
+    void putPixel(int x,int y,std::uint32_t color);
+    void drawLine(int x0,int y0,int x1,int y1,std::uint32_t color);
+    int width_,height_; std::vector<std::uint32_t> pixels_;
 };
