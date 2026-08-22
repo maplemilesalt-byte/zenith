c71ea81af2ad88e21314498cf8b379f853e33bee
Maple_Miles <maplemilesalt@gmail.com>
2026-08-22 16:25:14 -0300

    Use minimal CoreApplication host for Xbox UWP

--- platform/xbox/uwp/App.h
+++ platform/xbox/uwp/App.h
@@ -1,15 +1,22 @@
 #pragma once
 
 #include <winrt/Windows.ApplicationModel.Core.h>
+#include <winrt/Windows.Foundation.h>
 #include <winrt/Windows.UI.Core.h>
-#include <winrt/Windows.UI.Xaml.h>
 
 namespace zenith::xbox::uwp
 {
-    struct App final : winrt::Windows::UI::Xaml::ApplicationT<App>
+    struct App final : winrt::implements<App, winrt::Windows::ApplicationModel::Core::IFrameworkView>
     {
-        App();
+        void Initialize(winrt::Windows::ApplicationModel::Core::CoreApplicationView const& view);
+        void SetWindow(winrt::Windows::UI::Core::CoreWindow const& window);
+        void Load(winrt::hstring const& entryPoint);
+        void Run();
+        void Uninitialize();
+    };
 
-        void OnLaunched(winrt::Windows::ApplicationModel::Activation::LaunchActivatedEventArgs const& args);
+    struct AppSource final : winrt::implements<AppSource, winrt::Windows::ApplicationModel::Core::IFrameworkViewSource>
+    {
+        winrt::Windows::ApplicationModel::Core::IFrameworkView CreateView();
     };
 }
